a3b59eacff8013d62423ac54a24b9caa400d22be
Matthias Jost <8178852+matthiasjost@users.noreply.github.com>
2022-06-06 12:06:38 +0200

    Correct tests

--- DataCollector/DataCollector.Services.Tests/HtmlServiceTests.cs
+++ DataCollector/DataCollector.Services.Tests/HtmlServiceTests.cs
@@ -31,7 +31,24 @@ namespace DataCollector.Services.Tests
             {
                 var htmlService = new HtmlService();
                 htmlService.ParseRssAndAtomXmlLinks(httpService.ResponseString);
-                Assert.True(htmlService.ExtractedRssXmlLinks.Count > 1, "Didn't find any URLs in HTML code!");
+                Assert.True(htmlService.ExtractedRssXmlLinks.Count > 0, "Didn't find any URLs in HTML code!");
+            }
+            else
+            {
+                Assert.True(false, "Failed to load from Url!");
+            }
+        }
+        [Fact]
+        public async void TestIfYouTubeChannelExtractFeedUrls()
+        {
+            var httpService = new HttpService();
+
+            httpService.Url = "https://www.youtube.com/c/binarythistle";
+            if (await httpService.TryUrlToString())
+            {
+                var htmlService = new HtmlService();
+                await htmlService.ParseRssAndAtomXmlLinks(httpService.ResponseString);
+                Assert.True(htmlService.ExtractedRssXmlLinks.Count > 0, "Didn't find any URLs in HTML code!");
             }
             else
             {
