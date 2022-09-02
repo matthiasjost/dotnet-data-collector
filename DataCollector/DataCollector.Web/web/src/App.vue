18c1bb2a443e950d1c06a1cfd4c3a7261501d683
Matthias Jost <8178852+matthiasjost@users.noreply.github.com>
2022-09-02 15:28:52 +0200

    pico and view

---
+++ DataCollector/DataCollector.Web/web/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import CreatorView from "@/views/CreatorView.vue";
+</script>
+
+<template>
+<CreatorView />
+</template>
+<style scoped>
+</style>
--- DataCollector/DataCollector.Web/web/src/views/CreatorView.vue
+++ DataCollector/DataCollector.Web/web/src/views/CreatorView.vue
@@ -1,10 +1,51 @@
 <template>
-$END$
+  <main class="container">
+    <nav>
+      <ul>
+        <li><strong>content-creators.net</strong></li>
+      </ul>
+      <ul>
+        <li><a href="https://www.github.com/matthiasjost/dotnet-content-creators" role="button">GitHub</a></li>
+      </ul>
+    </nav>
+    <h1>.NET Content Creators</h1>
+    <div class="grid">
+      <div><input type="search" id="search" name="search" placeholder="Search"></div>
+    </div>
+    <table role="grid">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Country</th>
+        <th scope="col">Name</th>
+        <th scope="col">Channels</th>
+        <th scope="col">Last Check</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+      </tr>
+      </tbody>
+    </table>
+    <p>This list is based on the following GitHub Repository: <a href="https://github.com/matthiasjost/dotnet-content-creators">matthiasjost/dotnet-content-creators</a></p>
+  </main>
 </template>
 
 <script>
 export default {
-name: "CreatorView"
+  name: "CreatorView"
 }
 </script>
 
